d01b213760be9d4f4a47e789a8c9edc02bd64c2e
Liam <37994@ma-web.nl>
2024-06-27 21:15:55 +0200

    progress

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
+let darkmodeEnabled = localStorage.getItem('darkmode') == 'true';
+const root = document.documentElement;
 const darkmode = document.getElementById('js--darkmodeButton');
-let darkmodeEnabled = false;
 
-darkmode.addEventListener("click", function() {
-    darkmodeEnabled = !darkmodeEnabled;
-    document.documentElement.setAttribute('darkmode', darkmodeEnabled);
-});
+if (darkmode) {
+    darkmode.addEventListener("click", function() {
+        darkmodeEnabled = !darkmodeEnabled;
+        localStorage.setItem('darkmode', darkmodeEnabled);
+        root.setAttribute('darkmode', darkmodeEnabled);
+    });
+}
+
+if (darkmodeEnabled) {
+    root.setAttribute('darkmode', true);
+}
